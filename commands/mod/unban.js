cd455ebb442083018436f3ae057c47079eadab93
Kevin <kchen0296@gmail.com>
2021-05-13 15:39:57 -0400

    Fixed unban command

--- commands/mod/unban.js
+++ commands/mod/unban.js
@@ -4,9 +4,18 @@ module.exports = {
   usage: '!unban <userID>: Unbans user',
   args: true,
   execute(message, args) {
-    const guildMemberID = args[0];
+    const memberID = args[0];
     if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('BAN_USERS')) {
-      
+      if (!memberID) {
+        message.channel.send('Enter a valid user id');
+      }
+      try {
+        message.guild.members.unban(memberID);
+        message.channel.send(`Unbanned ${memberID}`);
+      } catch(err) {
+        console.log(err);
+        message.channel.send('Error unbanning');
+      }
     } 
   }
 }
