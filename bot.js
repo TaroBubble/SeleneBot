0d60f7b34675168aea0cf9bd5f5998f435149710
Kevin <kchen0296@gmail.com>
2021-05-21 20:10:34 -0400

    Added Youtube Video search

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const search = require('youtube-search');
 require('dotenv').config();
 
 const prefix = process.env.PREFIX;
--- commands/fun/youtube.js
+++ commands/fun/youtube.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
-const fetch = require('node-fetch');
+const search = require('youtube-search');
 const queryString = require('querystring');
+const { config } = require('dotenv');
+const options = {
+  maxResults: 5,
+  key: process.env.YOUTUBE_API,
+  type: 'video'
+}
 module.exports = {
   name: 'youtube',
   usage: '!youtube <search term>',
@@ -9,8 +15,17 @@ module.exports = {
     if (!args.length) {
       return message.reply('No argument provided!');
     }
-    
-    const query = queryString.stringify({term: args.join(' ')});
-    
+    let query = args.join(' ');
+    let embed = new Discord.MessageEmbed()
+      .setColor('#5cddbf')
+      .setTitle('Youtube Search')
+      .setDescription('Enter a search');
+    message.channel.send(embed);
+    message.channel.send(query);
+
+    search(query, options, function(err, results) {
+      if (err) return console.log
+      console.dir(results);
+    })
   }
 }
